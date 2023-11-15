6f8af67180d106d98e770706a413c356700aaddc
rintaro-s <120776920+rintaro-s@users.noreply.github.com>
2023-11-15 22:03:56 +0900

    Add files via upload

---
+++ いしころ/contentScript.js
@@ -0,0 +1,3 @@
+const logo = document.createElement("h2");
+logo.innerHTML = '<a href="https://youtube.com"><img src="https://cdn.discordapp.com/attachments/1099335406790639766/1174249015609659442/I.png?ex=6566e79d&is=6554729d&hm=3708d6c20fedf3f384c3e5c8f2948178924e736f3053ea937b4b7165be10599a&" alt="画像の説明" width="100" height="70"></a>';
+document.querySelector(".ytd-logo").appendChild(logo);
